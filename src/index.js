62ae79b1d9df36d9e50647f8dcaff5d09ac60f49
austinwilson1224 <austin1224@gmail.com>
2020-08-28 10:53:24 -0700

    finished

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./assets/style.css";
 import quizService from "./quizService";
+import QuestionBox from "./components/QuestionBox";
+import Result from "./components/Result";
 
 class QuizBee extends React.Component {
     state = {
-        questionBank: []
+        questionBank: [],
+        score: 0,
+        responses: 0
     };
     getQuestions = () => {
         quizService().then(question => {
@@ -19,11 +23,41 @@ class QuizBee extends React.Component {
         this.getQuestions();
     }
 
+    computeAnswer = (answer, correctAnswer) => {
+        if (answer === correctAnswer) {
+            this.setState({
+                score: this.state.score + 1
+            })
+        }
+        this.setState({
+            responses: this.state.responses < 5 ? this.state.responses + 1 : 5
+        })
+    }
+
+    playAgain = () => {
+        this.getQuestions();
+        this.setState({
+            score: 0,
+            responses: 0
+        })
+    }
+
     render() {
         return (
             <div className="container">
                 <div className="title">QuizBee</div>
-                {this.state.questionBank.length > 0 && this.state.questionBank.map( ( {question, answers, correct, questionId} )  => <h4>{question}</h4>)}
+                {this.state.questionBank.length > 0 && 
+                this.state.responses < 5 &&
+                this.state.questionBank.map( ( {question, answers, correct, questionId} )  => (
+                    <QuestionBox 
+                    question={question} 
+                    options={answers} 
+                    key={questionId} 
+                    selected={answer => this.computeAnswer(answer, correct)}
+                    />
+                ))}
+
+                {this.state.responses === 5 ? (<Result score={this.state.score} playAgain={this.playAgain} />) : null}
             </div>
         )
     }
