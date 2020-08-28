746aa20a127bbc756ec1913d3b22288193b109da
austinwilson1224 <austin1224@gmail.com>
2020-08-28 09:58:34 -0700

    react app

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./assets/style.css";
+
+class QuizBee extends React.Component {
+
+    render() {
+        return (
+            <div className="container">
+                <div className="title">QuizBee</div>
+            </div>
+        )
+    }
+}
+
+
+ReactDOM.render(<QuizBee />, document.getElementById("root"));
